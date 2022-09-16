56b475fa0812f208c8e915b5a5eb2c17561021a0
Bachir Bekkaye <bachirbk@gmail.com>
2022-09-16 13:55:15 -0700

    Add project files.

---
+++ RAASession07QA/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace RAASession07QA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ViewTemplateList = new System.Windows.Forms.CheckedListBox();
+            this.btnSelectAll = new System.Windows.Forms.Button();
+            this.btnDeselectAll = new System.Windows.Forms.Button();
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.btnCancel2 = new System.Windows.Forms.Button();
+            this.btnOK2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ViewTemplateList
+            // 
+            this.ViewTemplateList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ViewTemplateList.FormattingEnabled = true;
+            this.ViewTemplateList.Location = new System.Drawing.Point(16, 38);
+            this.ViewTemplateList.Name = "ViewTemplateList";
+            this.ViewTemplateList.Size = new System.Drawing.Size(524, 361);
+            this.ViewTemplateList.TabIndex = 0;
+            // 
+            // btnSelectAll
+            // 
+            this.btnSelectAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSelectAll.Location = new System.Drawing.Point(16, 426);
+            this.btnSelectAll.Name = "btnSelectAll";
+            this.btnSelectAll.Size = new System.Drawing.Size(71, 29);
+            this.btnSelectAll.TabIndex = 1;
+            this.btnSelectAll.Text = "+";
+            this.btnSelectAll.UseVisualStyleBackColor = true;
+            this.btnSelectAll.Click += new System.EventHandler(this.btnSelectAll_Click);
+            // 
+            // btnDeselectAll
+            // 
+            this.btnDeselectAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnDeselectAll.Location = new System.Drawing.Point(121, 426);
+            this.btnDeselectAll.Name = "btnDeselectAll";
+            this.btnDeselectAll.Size = new System.Drawing.Size(71, 29);
+            this.btnDeselectAll.TabIndex = 2;
+            this.btnDeselectAll.Text = "-";
+            this.btnDeselectAll.UseVisualStyleBackColor = true;
+            this.btnDeselectAll.Click += new System.EventHandler(this.btnDeselectAll_Click);
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Location = new System.Drawing.Point(13, 9);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(44, 16);
+            this.lblHeading.TabIndex = 5;
+            this.lblHeading.Text = "label1";
+            // 
+            // btnCancel2
+            // 
+            this.btnCancel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel2.Location = new System.Drawing.Point(447, 432);
+            this.btnCancel2.Name = "btnCancel2";
+            this.btnCancel2.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel2.TabIndex = 6;
+            this.btnCancel2.Text = "Cancel";
+            this.btnCancel2.UseVisualStyleBackColor = true;
+            // 
+            // btnOK2
+            // 
+            this.btnOK2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK2.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK2.Location = new System.Drawing.Point(334, 432);
+            this.btnOK2.Name = "btnOK2";
+            this.btnOK2.Size = new System.Drawing.Size(75, 23);
+            this.btnOK2.TabIndex = 7;
+            this.btnOK2.Text = "OK";
+            this.btnOK2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 485);
+            this.Controls.Add(this.btnOK2);
+            this.Controls.Add(this.btnCancel2);
+            this.Controls.Add(this.lblHeading);
+            this.Controls.Add(this.btnDeselectAll);
+            this.Controls.Add(this.btnSelectAll);
+            this.Controls.Add(this.ViewTemplateList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox ViewTemplateList;
+        private System.Windows.Forms.Button btnSelectAll;
+        private System.Windows.Forms.Button btnDeselectAll;
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Button btnCancel2;
+        private System.Windows.Forms.Button btnOK2;
+    }
+}
---
+++ RAASession07QA/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RAASession07QA
+{
+    public partial class Form1 : Form
+    {
+        public Form1(List<string> names, string label)
+        {           
+            InitializeComponent();
+
+            foreach(string name in names)
+            {
+                ViewTemplateList.Items.Add(name);
+            }
+
+            lblHeading.Text = label;
+        }
+
+
+        private void btnSelectAll_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < ViewTemplateList.Items.Count; i++)
+            {
+                ViewTemplateList.SetItemChecked(i, true);
+            }
+        }
+
+        private void btnDeselectAll_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < ViewTemplateList.Items.Count; i++)
+            {
+                ViewTemplateList.SetItemChecked(i, false);
+            }
+        }
+
+        public List<string> GetSelectedItems()
+        {
+            List<string> resultList = new List<string>();
+
+            foreach (string item in ViewTemplateList.CheckedItems)
+            {
+                resultList.Add(item);
+            }
+            return resultList;
+            
+        }
+    }
+}
